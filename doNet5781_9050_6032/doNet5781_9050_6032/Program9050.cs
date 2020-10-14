c22e41f16e08bcd0bc644904bbb4fd00979a91a5
david melnik <davideli@g.jct.ac.il>
2020-10-14 20:14:58 +0300

    initial code + welcome 6032

---
+++ doNet5781_9050_6032/doNet5781_9050_6032/Program9050.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doNet5781_9050_6032
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9050();
+            Wellcome6032();
+            //blabla
+        }
+
+        private static void Welcome9050()
+        {
+            Console.Write("Enter your name: ");
+            String userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Wellcome6032();
+    }
+}
