59d77bc1d8d8b2dec9473e8947b38c95f756bf23
againand <72875228+againand@users.noreply.github.com>
2020-10-21 18:42:46 +0300

    lesson

--- doNet5781_9050_6032/targil1/Program.cs
+++ doNet5781_9050_6032/targil1/Program.cs
@@ -3,24 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
-namespace targil1
+namespace Targil1
 {
-    public enum CHOICE
-    {
-        ADD_BUS, PICK_BUS, REFUKE_BUS, MAINTENACNE_BUS,  EXIT
-    }
-    
     class Program
     {
         static void Main(string[] args)
         {
-
-            int choice = -1;
+            List<Bus> buses = new List<Bus>();
+            CHOICE choice;
+            bool success = true;
             do
             {
 
-            } while (true);
+                success = Enum.TryParse(Console.ReadLine(), out choice);
+                if (!success)
+                {
+                    continue;
+                }
+                switch (choice)
+                {
+                    case CHOICE.ADD_BUS:
+                        Console.WriteLine("rishuy: ");
+                        string rishuy = Console.ReadLine();
+                        Console.WriteLine("taarich");
+                        DateTime taarich;
+                        success = DateTime.TryParse(Console.ReadLine(), out taarich);
+                        if (!success)
+                        {
+                            Console.WriteLine("Error");
+                            continue;
+                        }
+                        try
+                        {
+                            buses.Add(new Bus(rishuy, taarich));
+
+                        }
+                        catch (Exception baaya)
+                        {
+                            Console.WriteLine(baaya.Message);
+                        }
+                        //TODO
+                        break;
+                    case CHOICE.PICK_BUS:
+                        break;
+                    case CHOICE.REFUEL_BUS:
+                        break;
+                    case CHOICE.MAINTENANCE_BUS:
+                        break;
+                    case CHOICE.EXIT:
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (choice != CHOICE.EXIT);
         }
     }
 }
