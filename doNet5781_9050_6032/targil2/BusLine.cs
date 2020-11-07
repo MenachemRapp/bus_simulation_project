d24d37308d287cc66240f78300dcd25218d2e43d
againand <72875228+againand@users.noreply.github.com>
2020-11-07 21:51:19 +0200

    program from the lesson

---
+++ doNet5781_9050_6032/targil2/BusLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil2
+{
+    public class BusLine
+    {
+        public int BusNumber { get; set; }
+
+        private List<BusStopLine> stations = new List<BusStopLine>();
+
+        public List<BusStopLine> Stations
+        {
+            get
+            {
+                return stations;
+            }
+        }
+        public Area Area { get; set; }
+
+        public BusStopLine FirstStation { get => Stations[0]; set => Stations[0] = value; }
+        public BusStopLine LastStation { get => Stations[stations.Count - 1]; set => Stations[stations.Count - 1] = value; }
+
+    }
+}
---
+++ doNet5781_9050_6032/targil2/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil2
+{
+    public enum Area
+    {
+        Central,Jerusalem,North,South,Center
+    }
+}
