a4a8f36f0cb8984ca8ed900638c92c9b7de217fc
againand <72875228+againand@users.noreply.github.com>
2020-11-09 15:26:32 +0200

    titles for busLine metods

--- doNet5781_9050_6032/targil2/BusLine.cs
+++ doNet5781_9050_6032/targil2/BusLine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace targil2
 {
-    public class BusLine
+    public class BusLine:IComparable<BusLine>
     {
         public int BusNumber { get; set; }
 
@@ -24,5 +24,36 @@ namespace targil2
         public BusStopLine FirstStation { get => Stations[0]; set => Stations[0] = value; }
         public BusStopLine LastStation { get => Stations[stations.Count - 1]; set => Stations[stations.Count - 1] = value; }
 
+
+        public void add()
+        { }
+
+        public void remove()
+        { }
+
+        public bool findStop(BusStop val)
+        {
+            return false;
+        }
+
+        public double stopsDistance(BusStop first, BusStop last)
+        {
+            return 0;
+        }
+
+        public TimeSpan stopsTime(BusStop first, BusStop last)
+        {
+            return new TimeSpan {};
+        }
+
+        public BusStopLine subroute(BusStop first, BusStop last)
+        {
+            return new BusStopLine { };
+        }
+                   
+        public int CompareTo(BusLine other)
+        {
+            return stopsDistance(FirstStation,LastStation).CompareTo(other.stopsDistance(other.FirstStation, other.LastStation));
+        }
     }
 }
