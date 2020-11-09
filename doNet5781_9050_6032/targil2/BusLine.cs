cd511e798397718d81c245b514213682ff14b43d
againand <72875228+againand@users.noreply.github.com>
2020-11-09 17:03:28 +0200

    update busline

--- doNet5781_9050_6032/targil2/BusLine.cs
+++ doNet5781_9050_6032/targil2/BusLine.cs
@@ -24,36 +24,92 @@ namespace targil2
         public BusStopLine FirstStation { get => Stations[0]; set => Stations[0] = value; }
         public BusStopLine LastStation { get => Stations[stations.Count - 1]; set => Stations[stations.Count - 1] = value; }
 
+        public override string ToString()
+        {
+            return String.Format("[ {0}, {1} ]", BusNumber, Area);
+            //add list
+        }
 
-        public void add()
-        { }
+        public void add(BusStopLine newStop, int priveousStop)
+        {
+            //BusStopLine stop= new BusStopLine { }
+            //if
+            stations.Add(newStop);
+
+            //add to the middle or end of the list
+        //Todo
+        }
 
         public void remove()
-        { }
+        {
+            //Todo
+        }
 
         public bool findStop(BusStop val)
         {
+            foreach (BusStopLine stop in stations)
+                if (val.BusStationKey == stop.BusStationKey)
+                    return true;
             return false;
         }
 
         public double stopsDistance(BusStop first, BusStop last)
         {
-            return 0;
+            BusLine sub = subroute(first, last);
+            return sub.fullLineDistance();
         }
 
         public TimeSpan stopsTime(BusStop first, BusStop last)
         {
-            return new TimeSpan {};
+            BusLine sub = subroute(first, last);
+            return sub.fullLineTime();
+
         }
 
-        public BusStopLine subroute(BusStop first, BusStop last)
+        private TimeSpan fullLineTime()
         {
-            return new BusStopLine { };
+            TimeSpan sum = TimeSpan.Zero;
+            foreach (BusStopLine stop in stations)
+                sum += stop.Zman;
+
+            return sum;
+        }
+
+        private double fullLineDistance()
+        {
+            double sum = 0;
+            foreach (BusStopLine stop in stations)
+                sum += stop.Distance;
+
+            return sum;
+        }
+
+
+        private BusLine subroute(BusStop first, BusStop last)
+        {
+
+            BusLine subLine= new BusLine { };
+            bool flag = false;
+            foreach (BusStopLine stop in stations)
+            {
+                if (flag)
+                {
+                    subLine.stations.Add(stop);
+                    if (stop.BusStationKey == last.BusStationKey)
+                        break;
+                }
+                else if (stop.BusStationKey == first.BusStationKey)
+                    flag = true;
+            }
+                
+            return subLine;
         }
                    
         public int CompareTo(BusLine other)
         {
-            return stopsDistance(FirstStation,LastStation).CompareTo(other.stopsDistance(other.FirstStation, other.LastStation));
+            return fullLineTime().CompareTo(other.fullLineTime());
         }
+
+
     }
 }
