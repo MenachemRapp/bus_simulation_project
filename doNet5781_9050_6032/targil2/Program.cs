4eb9c81f3eb2be59d8158ec7b4de9b32a0ba17c7
againand <72875228+againand@users.noreply.github.com>
2020-11-10 21:11:26 +0200

    undo

--- doNet5781_9050_6032/targil2/Program.cs
+++ doNet5781_9050_6032/targil2/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace targil2
@@ -11,18 +11,62 @@ namespace targil2
     {
         static void Main(string[] args)
         {
-            BusLine play = new BusLine();
-            BusStop first = new BusStop(1, 34, 50, "a");
-            BusStop last = new BusStop(4, 34.0, 43.0, "a");
-            play.add(1, 34, 50, "a", 0, TimeSpan.FromMinutes(0), 0);
-            play.add(2, 34, 43, "b", 1, TimeSpan.FromMinutes(21), 1);
-            play.add(3, 34, 43, "c", 2, TimeSpan.FromMinutes(21), 2);
-            play.add(4, 34, 43, "d", 3, TimeSpan.FromMinutes(21), 3);
-            play.remove(first);
-
-
-            //Console.WriteLine(play.stopsDistance(first,last));
-
+            CHOICE choice; ;
+            switch (choice)
+            {
+                case CHOICE.ADD:
+                    ADD add;
+                    switch (add)
+                    {
+                        case ADD.ADD_LINE:
+                            break;
+                        case ADD.ADD_BUS_STOP:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.REMOVE:
+                    REMOVE remove;
+                    switch (remove)
+                    {
+                        case REMOVE.REMOVE_LINE:
+                            break;
+                        case REMOVE.REMOVE_BUS_STOP:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.FIND:
+                    FIND find;
+                    switch (find)
+                    {
+                        case FIND.FIND_LINES_IN_STOP:
+                            break;
+                        case FIND.OPTIONS_BETWEEN_STOPS:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.PRINT:
+                    PRINT print;
+                    switch (print)
+                    {
+                        case PRINT.PRINT_LINES:
+                            break;
+                        case PRINT.PRINT_STOPS:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.EXIT:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
