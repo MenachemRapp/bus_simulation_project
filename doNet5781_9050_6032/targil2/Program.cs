82f6944fd325292e0cb7e5d1c68f39e923b6b64c
david melnik <davideli@g.jct.ac.il>
2020-11-10 20:44:25 +0200

    david4

--- doNet5781_9050_6032/targil2/Program.cs
+++ doNet5781_9050_6032/targil2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace targil2
@@ -10,6 +11,62 @@ namespace targil2
     {
         static void Main(string[] args)
         {
+            CHOICE choice; ;
+            switch (choice)
+            {
+                case CHOICE.ADD:
+                    PRINT print;
+                    switch (print)
+                    {
+                        case PRINT.PRINT_LINES:
+                            break;
+                        case PRINT.PRINT_STOPS:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.REMOVE:
+                    REMOVE remove;
+                    switch (remove)
+                    {
+                        case REMOVE.REMOVE_LINE:
+                            break;
+                        case REMOVE.REMOVE_BUS_STOP:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.FIND:
+                    FIND find;
+                    switch (find)
+                    {
+                        case FIND.FIND_LINES_IN_STOP:
+                            break;
+                        case FIND.OPTIONS_BETWEEN_STOPS:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.PRINT:
+                    PRINT print;
+                    switch (print)
+                    {
+                        case PRINT.PRINT_LINES:
+                            break;
+                        case PRINT.PRINT_STOPS:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case CHOICE.EXIT:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
