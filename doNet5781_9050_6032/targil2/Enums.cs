fbd374cd7bd404a41c441945e60769951b23f35b
againand <72875228+againand@users.noreply.github.com>
2020-11-10 20:35:36 +0200

    enum

--- doNet5781_9050_6032/targil2/Enums.cs
+++ doNet5781_9050_6032/targil2/Enums.cs
@@ -10,6 +10,27 @@ namespace targil2
     {
         Central,Jerusalem,North,South,Center
     }
+    public enum CHOICE
+    {
+        ADD,REMOVE, FIND,PRINT, EXIT = -1
+    }
 
+    public enum ADD
+    {
+        ADD_LINE, ADD_BUS_STOP
+    }
 
+    public enum REMOVE
+    {
+        REMOVE_LINE, REMOVE_BUS_STOP
+    }
+
+    public enum FIND
+    {
+        FIND_LINES_IN_STOP, OPTIONS_BETWEEN_STOPS
+    }
+        public enum PRINT
+    {
+        PRINT_LINES, PRINT_STOPS
+    }
 }
