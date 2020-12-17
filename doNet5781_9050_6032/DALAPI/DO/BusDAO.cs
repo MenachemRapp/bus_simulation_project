078f9946c58508859cf0f40bb9f938b75b941b6a
david melnik <davideli@g.jct.ac.il>
2020-12-17 16:12:26 +0200

    stage 1 david melnik do

---
+++ doNet5781_9050_6032/DALAPI/DO/BusDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    class BusDAO
+    {
+         int id { get; set; }
+        DateTime LicensingDate { get; set; }
+        int mileage { get; set; }
+        int AmountFuel { get; set; }
+        BUS_STATUS status { get; set; }
+
+        public override string ToString()
+        {
+            return this.ToStringProperty();
+        }
+
+    }
+}
---
+++ doNet5781_9050_6032/DALAPI/DO/Enumim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+
+    
+    public enum BUS_STATUS
+    {
+        DRIVING, REFULING, FIXING, AVAILABLE, NOT_MAINTAINED
+    }
+}
