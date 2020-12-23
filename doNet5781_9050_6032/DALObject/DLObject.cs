cc7a1c6b3f3c3783a50a2ef28b27bca30d39e3e2
againand <72875228+againand@users.noreply.github.com>
2020-12-23 18:58:59 +0200

    more

--- doNet5781_9050_6032/DALObject/DLObject.cs
+++ doNet5781_9050_6032/DALObject/DLObject.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DALAPI;
 
 namespace DL
 {
-    internal class DALObject
+    internal class DLObject:IDL
     {
     }
 }
