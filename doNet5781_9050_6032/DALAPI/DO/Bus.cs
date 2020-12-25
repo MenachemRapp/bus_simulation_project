dfafb741d7b8f5ff647b720d078cfd96c8d28f1a
david melnik <davideli@g.jct.ac.il>
2020-12-25 12:59:28 +0200

    start_of_idl

--- doNet5781_9050_6032/DALAPI/DO/Bus.cs
+++ doNet5781_9050_6032/DALAPI/DO/Bus.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    class BusDAO
+    class Bus
     {
         public int license_num { get; set; }
         public DateTime LicensingDate { get; set; }
--- doNet5781_9050_6032/DALAPI/IDL.cs
+++ doNet5781_9050_6032/DALAPI/IDL.cs
@@ -9,5 +9,15 @@ namespace DALAPI
 {
     public interface IDL
     {
+        #region Bus
+        
+        IEnumerable<DO.Bus> GetAllBuss();
+        IEnumerable<DO.Bus> GetAllBussBy(Predicate<DO.Bus> predicate);
+        DO.Bus GetBus(int id);
+        void AddBus(DO.Bus bus);
+        void UpdateBus(DO.Bus bus);
+        void UpdateBus(int id, Action<DO.Bus> update); //method that knows to updt specific fields in Bus
+        void DeleteBus(int id);
+        #endregion
     }
 }
