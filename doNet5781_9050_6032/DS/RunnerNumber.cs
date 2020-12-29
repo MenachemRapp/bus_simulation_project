f37090916169fbf91eccbf688206ebc0be60e8a0
david melnik <davideli@g.jct.ac.il>
2020-12-29 21:03:30 +0200

    tiny

---
+++ doNet5781_9050_6032/DS/RunnerNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public static class RunnerNumber
+    {
+        private static int id_bus_on_trip = 1;
+        private static int id_line = 1;
+        private static int id_trip = 1;
+
+        public static int GetIdBusOnTrip(){return id_bus_on_trip++; }
+        public static int GetIdLine() { return id_line++; }
+        public static int GetIdTrip() { return id_trip++; }
+    }
+}
