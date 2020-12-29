5f0f0859e7386c7672d1455109a0baa726d49912
david melnik <davideli@g.jct.ac.il>
2020-12-29 23:10:17 +0200

    IDL MORE

--- doNet5781_9050_6032/DALAPI/IDL.cs
+++ doNet5781_9050_6032/DALAPI/IDL.cs
@@ -41,6 +41,47 @@ namespace DALAPI
 
         #endregion
 
+        #region BusOnTrip
+        IEnumerable<DO.BusOnTrip> GetAllBusOnTrips();
+        IEnumerable<DO.BusOnTrip> GetAllBusOnTripsBy(Predicate<DO.BusOnTrip> predicate);
+        DO.BusOnTrip GetBusOnTrip(int LicenseNum, int LineId, TimeSpan PlannedTakeOff);
+        DO.BusOnTrip GetBusOnTrip(int Id);
+        void AddBusOnTrip(DO.BusOnTrip bus_on_trip);
+        void UpdateBusOnTrip(DO.BusOnTrip bus_on_trip);
+        void UpdateBusOnTrip(int LicenseNum, int LineId, TimeSpan PlannedTakeOff, Action<DO.BusOnTrip> update); //method that knows to updt specific fields in BusOnTrip
+        void UpdateBusOnTrip(int Id, Action<DO.BusOnTrip> update); //method that knows to updt specific fields in BusOnTrip
+        void DeleteBusOnTrip(int LicenseNum, int LineId, TimeSpan PlannedTakeOff);
+        void DeleteBusOnTrip(int Id);
+        #endregion
+
+        #region Line
+        IEnumerable<DO.Line> GetAllLines();
+        IEnumerable<DO.Line> GetAllLinesBy(Predicate<DO.Line> predicate);
+        DO.Line GetLine(int Id);
+        void AddLine(DO.Line line);
+        void UpdateLine(DO.Line line);
+        void UpdateLine(int Id, Action<DO.Line> update); //method that knows to updt specific fields in Line
+        void DeleteLine(int Id);
+        #endregion
+
+        //#region LineTrip
+        //IEnumerable<DO.LineTrip> GetAllLineTrips();
+        //IEnumerable<DO.LineTrip> GetAllLineTripsBy(Predicate<DO.LineTrip> predicate);
+        //DO.LineTrip GetLineTrip(int LicenseNum);
+        //void AddLineTrip(DO.LineTrip line_trip);
+        //void UpdateLineTrip(DO.LineTrip line_trip);
+        //void UpdateLineTrip(int LicenseNum, Action<DO.LineTrip> update); //method that knows to updt specific fields in LineTrip
+        //void DeleteLineTrip(int LicenseNum);
+        //#endregion
+
+
+
+
+
+
+
+
+
 
 
 
